81419b87015ecd84d61b8efddf93eefdbda4cce6
czheng <czheng@fortinet.com>
2025-06-12 11:27:57 -0700

    status col

---
+++ lua/plugins/statuscol.lua
@@ -0,0 +1,36 @@
+return {
+  "luukvbaal/statuscol.nvim",
+  config = function()
+    -- Custom function to show both absolute and relative line numbers
+    local function lnum_both(args)
+      return string.format("%3d %2d", args.lnum, args.relnum)
+    end
+    require("statuscol").setup({
+      setopt = true,
+      segments = {
+        -- Git signs first
+        {
+          sign = {
+            namespace = { "gitsigns.*" },
+            name = { "gitsigns.*" },
+          },
+        },
+        -- Diagnostics second
+        {
+          sign = {
+            namespace = { ".*" },
+            name = { ".*" },
+            -- maxwidth = 2,
+            auto = true,
+          },
+        },
+        -- Line number last
+        {
+          text = { lnum_both, " " },
+          condition = { true },
+          click = "v:lua.ScLa",
+        },
+      },
+    })
+  end,
+}
