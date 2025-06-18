3d6a4e02b39a868739b060597738528683162019
czheng <czheng@fortinet.com>
2025-06-18 09:33:37 -0700

    warning fix

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,14 +1,32 @@
-return {
-  {
+--return {
+ --[[ {
     "folke/which-key.nvim",
     opts = {
       defaults = {
-        ["<leader>z"] = {
-          name = "Fzf", -- this is the sub-group name for <leader>z
-          g = { name = "Grep String" },
-          l = { name = "Live Grep" },
-        },
+        { "<leader>z", group = "Fzf" },
+        { "<leader>zg", group = "Grep String" },
+        { "<leader>zl", group = "Live Grep" },
       },
     },
-  }
+  }]
+  local wk = require("which-key")
+  wk.add({
+    { "<leader>z", group = "Fzf"},
+    { "<leader>zg", group = "Grep String" },
+    { "<leader>zl", group = "Live Grep" },
+  })
+}]]
+return {
+  {
+    "folke/which-key.nvim",
+    config = function()
+      local wk = require("which-key")
+      wk.add({
+        { "<leader>z", group = "Fzf"},
+        { "<leader>zg", group = "Grep String" },
+        { "<leader>zl", group = "Live Grep" },
+      })
+    end,
+  },
 }
+
