ebe6724118e957f9ef0716f8f0644a9d3fee87e5
czheng <czheng@fortinet.com>
2025-06-25 17:04:07 -0700

    color change escape chars json

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -2,6 +2,13 @@ return {
   { "catppuccin/nvim", name = "catppuccin", lazy = false,
     opts = {
     term_colors = true,
+    highlight_overrides = {
+        all = function()
+          return {
+            ["@string.escape.json"] = { fg = "#cba6f7", style = { "italic" } },
+          }
+        end,
+      }
     }
   },
   { "folke/tokyonight.nvim", lazy = false },
