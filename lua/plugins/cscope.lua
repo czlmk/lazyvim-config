9cba33b2d0eadaa26d0f4b8c8670cac23aa31dc7
czheng <czheng@fortinet.com>
2025-07-04 10:34:44 -0700

    event change cscope

--- lua/plugins/cscope.lua
+++ lua/plugins/cscope.lua
@@ -1,7 +1,6 @@
 return {
     {
         "dhananjaylatkar/cscope_maps.nvim",
-        event = "VeryLazy",
         dependencies = {
             -- "nvim-telescope/telescope.nvim", -- optional [for picker="telescope"]
             "ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
