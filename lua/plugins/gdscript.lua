45f3e97d1eac2a84af14d366d708bb92afd482b8
Clark Zheng <yiming.zheng@mail.mcgill.ca>
2025-07-13 16:21:55 -0700

    lspgdwsl set to not on mac

--- lua/plugins/gdscript.lua
+++ lua/plugins/gdscript.lua
@@ -1,10 +1,14 @@
-return {
-    "neovim/nvim-lspconfig",
-    opts = {
-        servers = {
-            gdscript = {
-                cmd = { "godot-wsl-lsp", "--useMirroredNetworking" },
+if vim.fn.has("macunix") == 0 then
+    return {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                gdscript = {
+                    cmd = { "godot-wsl-lsp", "--useMirroredNetworking" },
+                },
             },
         },
-    },
-}
+    }
+else
+    return {}
+end
